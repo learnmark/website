b916dabcd8cd7111b7249a5a92ba9621ada8d273
Wilson Wu <iwilsonwu@gmail.com>
2024-08-02 18:09:22 +0800

    add template

--- internal/entities/consultant.go
+++
@@ -1,9 +0,0 @@
-package entities
-
-type Consultant struct {
-	Base
-	Name         string `yaml:"name"`
-	Title        string `yaml:"title"`
-	Introduction string `yaml:"introduction"`
-	AvatarUrl    string `yaml:"AvatarUrl"`
-}
--- ui/src/views/Empty.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <v-container>
-    <h1>Comming Soon...</h1>
-  </v-container>
-</template>
