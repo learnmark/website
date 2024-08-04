7af33890c32c11e28249b370cca17a5645cab8db
Wilson Wu <iwilsonwu@gmail.com>
2024-08-05 00:18:43 +0800

    add logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,9 +5,9 @@ export default function Footer() {
       <div className="mx-auto max-w-screen-xl">
           <div className="md:flex md:justify-between">
               <div className="mb-6 md:mb-0">
-                  <a href="https://flowbite.com" className="flex items-center">
-                      <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-8" alt="FlowBite Logo" />
-                      <span className="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
+                  <a href="/" className="flex items-center">
+                      <img src="/logo.svg" className="mr-3 h-8" alt="Learnmark Logo" />
+                      <span className="self-center text-2xl font-semibold whitespace-nowrap">Learnmark</span>
                   </a>
               </div>
               <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -48,7 +48,7 @@ export default function Footer() {
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
           <div className="sm:flex sm:items-center sm:justify-between">
-              <span className="text-sm text-gray-500 sm:text-center">© 2022 <a href="https://flowbite.com" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+              <span className="text-sm text-gray-500 sm:text-center">© 2024 <a href="https://learnmark.com" className="hover:underline">Learnmark</a>. All Rights Reserved.
               </span>
               <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
                   <a href="#" className="text-gray-500 hover:text-gray-900 ">
