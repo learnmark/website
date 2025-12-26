f8fc0d76c85b9c40c18e00c0040972c7b82c58cb
Wilson Wu <iwilsonwu@gmail.com>
2025-12-26 12:53:48 +0800

    feat: remove "GatePoint" option from interest selection in contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -182,7 +182,6 @@ export default function Example() {
                 className="block w-full rounded-md border-0 pl-3.5 pr-9 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               >
                 <option disabled>Please Select</option>
-                <option>GatePoint</option>
                 <option>LetScrum</option>
                 <option>Solution / Service</option>
                 <option>Others</option>
