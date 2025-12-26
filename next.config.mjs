44659832fa02c7a99eaf33c2b4dba0d59b587c6d
Wilson Wu <iwilsonwu@gmail.com>
2025-12-26 14:09:17 +0800

    feat: add GitHub Pages deployment workflow and update Next.js configuration for export mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
+    output: process.env.BUILD_OUTPUT_MODE === 'export' ? 'export' : 'standalone',
+    images: {
+        unoptimized: process.env.BUILD_OUTPUT_MODE === 'export',
+    },
 };
 
 export default nextConfig;
